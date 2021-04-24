81ed45c9a0997507b9c4ad68c90daafe64ae8a67
Adryan Yudhatrisna <ayudhatrisna1@gmail.com>
2021-04-23 19:44:54 -0700

    Add product screen

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -17,7 +17,7 @@ const products = [
     name: 'iPhone 12 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Introducing the iPhone 12 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     brand: 'Apple',
     category: 'Electronics',
     price: 999.99,
